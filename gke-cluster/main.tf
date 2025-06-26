690865a8ddabd0c087830c1ab0faead2c0cb5caf
MuraliMOhan <muralisocial123@gmail.com>
2025-06-26 23:26:48 +0530

    Comitted

--- gke-cluster/main.tf
+++ gke-cluster/main.tf
@@ -1,15 +1,31 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  backend "gcs" {
+    bucket = "murali-test-devops1"         # ✅ Your existing bucket
+    prefix = "gke-cluster/terraform-state" # ✅ Folder path inside the bucket
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
 provider "google" {
   credentials = file("C:/Users/mural/Documents/gcp-terraform-key.json") # Update path as needed
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
   zone        = "asia-south1-a"
 }
+
 module "gke" {
-  source            = "./modules/gke"
-  project_id        = var.project_id
-  region            = var.region
-  cluster_name      = var.cluster_name
-  location          = var.location
+  source             = "./modules/gke"
+  project_id         = var.project_id
+  region             = var.region
+  cluster_name       = var.cluster_name
+  location           = var.location
   initial_node_count = var.initial_node_count
-  machine_type      = var.machine_type
+  machine_type       = var.machine_type
 }
