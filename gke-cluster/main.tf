fc741dac6429e071ab3057fd9a1253e190be5c26
smartCoder <smartcoder8295@gmail.com>
2025-06-27 15:20:17 +0530

    Committed

--- gke-cluster/main.tf
+++ gke-cluster/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "gcs" {
-    bucket = "sanjay_test_dev_bucket"         # ✅ Your existing bucket
+    bucket = "sanjay_test_dev_bucket"      # ✅ Your existing bucket
     prefix = "gke-cluster/terraform-state" # ✅ Folder path inside the bucket
   }
 
